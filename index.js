3b0bd2893654c7e2ba4a5c7a544be03b6b848727
JRaisala <juha.raisala@gmail.com>
2015-11-30 09:58:46 +0200

    MongoDB project started

---
+++ index.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require('body-parser');
+var database = require('./modules/database.js');
+var queries = require('./modules/queries.js');
+var person = require('./modules/person.js');
